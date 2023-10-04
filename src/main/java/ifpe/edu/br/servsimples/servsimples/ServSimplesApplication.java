1690d575acb7b38331579a72fbc500a315f9c6c9
Willian jefferson dos Santos <willjsan@motorola.com>
2023-10-04 12:03:25 -0300

    Iniciando projeto vazio;

---
+++ src/main/java/ifpe/edu/br/servsimples/servsimples/ServSimplesApplication.java
@@ -0,0 +1,13 @@
+package ifpe.edu.br.servsimples.servsimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServSimplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServSimplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ifpe/edu/br/servsimples/servsimples/ServSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ifpe.edu.br.servsimples.servsimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServSimplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
